f2f6a702b986cd89bd8cfe8d0fd6a2cfa896125f
liweiwei <18720594171@163.com>
2018-01-05 20:09:34 +0800

    Test Mac Commit

--- src/main/java/com/lww/design/graduation/Info.java
+++ src/main/java/com/lww/design/graduation/Info.java
@@ -6,6 +6,7 @@ package com.lww.design.graduation;
  */
 public class Info {
     public static void main(String[] args) {
+        System.out.println("Mac Test");
         System.out.println("lww graduation design");
     }
 }
