041d960462a15d8c194954148992c9315ccf4fb8
liweiwei <liweiwei@youzan.com>
2018-02-13 13:37:57 +0800

    info

--- src/main/java/com/lww/design/graduation/Info.java
+++ src/main/java/com/lww/design/graduation/Info.java
@@ -1,5 +1,7 @@
 package com.lww.design.graduation;
 
+import java.util.Date;
+
 /**
  * @author lww
  * @date 2017/12/12
@@ -8,5 +10,7 @@ public class Info {
     public static void main(String[] args) {
         System.out.println("Mac Test");
         System.out.println("lww graduation design");
+        System.out.println(new Date().getTime());
+
     }
 }
