061d54f2116107ed68a0e32a43ce1964882d1d32
liweiwei <liweiwei@youzan.com>
2018-02-13 18:04:50 +0800

    添加redis

---
+++ src/main/java/com/lww/design/graduation/utils/RedisUtil.java
@@ -0,0 +1,21 @@
+package com.lww.design.graduation.utils;
+
+import org.springframework.data.redis.core.RedisTemplate;
+
+public class RedisUtil {
+    private volatile RedisTemplate<String, String> redisTemplate;
+    public RedisUtil() {
+        super();
+    }
+    public RedisUtil(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public RedisTemplate<String, String> getRedisTemplate() {
+        return redisTemplate;
+    }
+
+    public void setRedisTemplate(RedisTemplate<String, String> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+}
