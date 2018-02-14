e4f057718c5bca8c51b5385b177c762c070912fa
liweiwei <liweiwei@youzan.com>
2018-02-14 10:52:24 +0800

    删除多余代码

--- src/main/java/com/lww/design/graduation/utils/RedisUtil.java
+++ src/main/java/com/lww/design/graduation/utils/RedisUtil.java
@@ -1,58 +1,21 @@
 package com.lww.design.graduation.utils;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.serializer.SerializerFeature;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.data.redis.serializer.SerializationException;
-
-import java.nio.charset.Charset;
 
 public class RedisUtil {
-    private volatile RedisTemplate<String, String> redisTemplate;
+    private volatile RedisTemplate<String,String> redisTemplate;
     public RedisUtil() {
         super();
     }
-    public RedisUtil(RedisTemplate<String, String> redisTemplate) {
-//        FastJsonRedisSerializer<Object> serializer = new FastJsonRedisSerializer<>(Object.class);
-//        redisTemplate.setDefaultSerializer(serializer);
-//        redisTemplate.afterPropertiesSet();
+    public RedisUtil(RedisTemplate<String,String> redisTemplate) {
         this.redisTemplate = redisTemplate;
     }
-
-    public RedisTemplate<String, String> getRedisTemplate() {
+    
+    public RedisTemplate<String,String> getRedisTemplate() {
         return redisTemplate;
     }
-    private static class FastJsonRedisSerializer<T> implements RedisSerializer<T> {
 
-        private static final Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
-
-        private Class<T> clazz;
-
-        FastJsonRedisSerializer(Class<T> clazz) {
-            this.clazz = clazz;
-        }
-
-        @Override
-        public byte[] serialize(T t) throws SerializationException {
-            if (t == null) {
-                return new byte[0];
-            }
-            return JSON.toJSONString(t).getBytes(DEFAULT_CHARSET);
-        }
-
-        @Override
-        public T deserialize(byte[] bytes) throws SerializationException {
-            if (bytes == null || bytes.length <= 0) {
-                return null;
-            }
-            String str = new String(bytes, DEFAULT_CHARSET);
-            return JSON.parseObject(str, clazz);
-        }
-    }
-
-    public void setRedisTemplate(RedisTemplate<String, String> redisTemplate) {
+    public void setRedisTemplate(RedisTemplate<String,String> redisTemplate) {
         this.redisTemplate = redisTemplate;
     }
 }
