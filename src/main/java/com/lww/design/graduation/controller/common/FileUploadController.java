ad4ab85195d7c8be91300aa4f890c5af811b2a37
liweiwei <liweiwei@youzan.com>
2018-02-19 18:22:21 +0800

    提交日志

--- src/main/java/com/lww/design/graduation/controller/common/FileUploadController.java
+++ src/main/java/com/lww/design/graduation/controller/common/FileUploadController.java
@@ -1,22 +1,16 @@
 package com.lww.design.graduation.controller.common;
 
-import com.alibaba.fastjson.JSON;
 import com.lww.design.graduation.common.AppConstant;
-import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
 import java.io.*;
-import java.util.Collections;
 
 @RestController
 public class FileUploadController {
